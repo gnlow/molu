b521d8428381b14b2bf555ff8b0c154562518c42
Gnlow <gnlow@protonmail.com>
2024-02-14 01:08:31 +0900

    generalize

--- mod.ts
+++ mod.ts
@@ -1,23 +1,38 @@
-type Elem = {
-    [k: string]: (str: TemplateStringsArray) => Elem
-    (str: TemplateStringsArray): Record<string, string>
+type Ctx<T> = {
+    [k: string]: (str: TemplateStringsArray) => Ctx<T>
+    (str: TemplateStringsArray): T
+}
+interface Elem<T> {
+    addAttribute(key: string | symbol, val: string): (data: T) => T 
 }
 
-const procgen = (obj: Record<string, string>): Elem => {
-    return new Proxy(
-        Object.assign(
-            () => obj,
-            obj,
-        ) as unknown as Elem,
-        {
-            get(target, prop, reciever) {
-                return ([val]: TemplateStringsArray) => procgen({
-                    ...obj,
-                    [prop]: val,
-                })
-            }
-        },
-    )
+const ctxGenGen =
+<T>(impl: Elem<T>) => {
+    const f = (data: T): Ctx<T> =>
+    {
+        return new Proxy(
+            Object.assign(
+                () => data,
+                data,
+            ) as unknown as Ctx<T>,
+            {
+                get(_target, prop, _reciever) {
+                    return ([val]: TemplateStringsArray) => f(
+                        impl.addAttribute(prop, val)(data)
+                    )
+                }
+            },
+        )
+    }
+    return f
 }
+const elemGen = ctxGenGen<Record<string, string>>({
+    addAttribute:
+    (k, v) =>
+    data => ({
+        ...data,
+        [k]: v,
+    })
+})
 
-export const div = procgen({})
+export const div = elemGen({})
